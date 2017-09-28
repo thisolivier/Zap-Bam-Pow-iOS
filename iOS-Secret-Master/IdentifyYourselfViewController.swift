21f818fad066c30ddde90b4f22e00fa2c6f25aa7
Olivier Butler <olivier.butler@gmail.com>
2017-09-27 23:55:15 -0700

    Finishing work on this particular failure

--- iOS-Secret-Master/IdentifyYourselfViewController.swift
+++ iOS-Secret-Master/IdentifyYourselfViewController.swift
@@ -10,16 +10,18 @@
 
 import UIKit
 import AVFoundation
+import Vision
 
-class IdentifyYourselfViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate{
+class IdentifyYourselfViewController: UIViewController{
     /******************/
     /* Initialization */
     /******************/
     var delegate:GameViewController?
-    var captureSession:AVCaptureSession?
     var videoPreviewLayer:AVCaptureVideoPreviewLayer?
     var qrCodeFrameView:UIView?
-    var myselfReal:AVCaptureMetadataOutputObjectsDelegate?
+    var captureSession:AVCaptureSession?
+    var videoOutput: AVCaptureVideoDataOutput?
+    var qRRequest:VNDetectBarcodesRequest?
     
     @IBOutlet weak var cameraDisplayView: UIView!
     @IBOutlet weak var nameLabelOutput: UILabel!
@@ -27,12 +29,9 @@ class IdentifyYourselfViewController: UIViewController, AVCaptureMetadataOutputO
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraDisplayView.layer.masksToBounds = true
-        myselfReal = self
-        if prepareForVideoCapture() {
-            setupQRCodeDetectionBox()
-        } else {
-            print("We were unable to start the capture session")
-        }
+        (cameraDisplayView as! CameraFrameView).delegate = self
+        //setupVisionRequest()
+        prepareForVideoCapture()
         
         if let realDelegate = delegate {
             if realDelegate.myName != "NaName" {
@@ -45,24 +44,25 @@ class IdentifyYourselfViewController: UIViewController, AVCaptureMetadataOutputO
     /* Setting up Camera */
     /*********************/
     
-    func prepareForVideoCapture() -> Bool{
+    func prepareForVideoCapture(){
         print(" ")
         print("Starting Camera Setup")
         let videoDevices = AVCaptureDevice.DiscoverySession.init(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera, AVCaptureDevice.DeviceType.builtInDualCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.back)
         let captureDevice = videoDevices.devices[0]
+        let captureMetadataOutput = AVCaptureMetadataOutput()
         
         do {
             // We start a capture session
             let input = try AVCaptureDeviceInput(device: captureDevice)
             captureSession = AVCaptureSession()
             captureSession?.addInput(input)
-            let captureMetadataOutput = AVCaptureMetadataOutput()
             
-            // We setup storage for session output
+            // Setup the output
             captureSession?.addOutput(captureMetadataOutput)
-            captureMetadataOutput.setMetadataObjectsDelegate(myselfReal!, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            let newQueue = DispatchQueue(label: "MyQueue")
+            captureMetadataOutput.setMetadataObjectsDelegate(cameraDisplayView as? AVCaptureMetadataOutputObjectsDelegate, queue: newQueue)
             
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
             
             // Now we attach the video being captured to a view
             videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
@@ -71,53 +71,47 @@ class IdentifyYourselfViewController: UIViewController, AVCaptureMetadataOutputO
             cameraDisplayView.layer.addSublayer(videoPreviewLayer!)
             captureSession?.commitConfiguration()
             captureSession?.startRunning()
-            
-            return true
+            print("Camera is running")
         } catch {
             print("We were unable to make an AVCaptureDevice using the front camera")
             print(error)
         }
-        return false
     }
     
-    func setupQRCodeDetectionBox(){
-        qrCodeFrameView = UIView()
-        if let qrCodeFrameView = qrCodeFrameView {
-            // Note that this view will not be visible until we detect a code
-            print("Adding qr box template")
-            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
-            qrCodeFrameView.layer.borderWidth = 2
-            cameraDisplayView.addSubview(qrCodeFrameView)
-            cameraDisplayView.bringSubview(toFront: qrCodeFrameView)
-        }
-    }
+    /************************/
+    /* The QR Functionality */
+    /************************/
     
-    /*********************/
-    /* Capturing QR Code */
-    /*********************/
+//    // Setup for a barcode detector object, which will scan for barcodes, and process the results
+//    func setupVisionRequest(){
+//        qRRequest = VNDetectBarcodesRequest(completionHandler: {
+//            (request, error) in
+//            // Loop through the found results
+//            for result in request.results! {
+//                // Cast the result to a barcode-observation
+//                if let barcode = result as? VNBarcodeObservation {
+//                    // Get the bounding box for the bar code and find the center
+//                    var rect = barcode.boundingBox
+//                    rect = rect.applying(CGAffineTransform(scaleX: 1, y: -1))
+//                    rect = rect.applying(CGAffineTransform(translationX: 0, y: 1))
+//                    let center = CGPoint(x: rect.midX, y: rect.midY)
+//                    print ("Payload: \(barcode.payloadStringValue!) at \(center)")
+//                }
+//            }
+//        })
+//    }
+//
+//    // Gets the current image from the ARSCNView (Augmented Reality Scene View) and makes an Image Request Handler using that image.
+//    // It then calls the handler's perform method, and passes it the request we made earlier.
+//    @objc func detectQR(){
+//        let cameraCurrent = videoPreviewLayer!
+//        let visionImageHandler = VNImageRequestHandler(cvPixelBuffer: cameraCurrent!, options: [.properties : ""])
+//        guard let _ = try? visionImageHandler.perform([qRRequest!]) else {
+//            return print("Could not perform barcode-request!")
+//        }
+//    }
     
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        print("We haz output")
-        // Check if the metadataObjects array is not nil and it contains at least one object.
-        if metadataObjects == nil || metadataObjects.count == 0 {
-            qrCodeFrameView?.frame = CGRect.zero
-            nameLabelOutput.text = "No QR code is detected"
-            return
-        }
-        
-        // Get the metadata object.
-        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
-        if metadataObj.type == AVMetadataObject.ObjectType.qr {
-            print(metadataObj)
-            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
-            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
-            qrCodeFrameView?.frame = barCodeObject!.bounds
-            
-            if metadataObj.stringValue != nil {
-                nameLabelOutput.text = metadataObj.stringValue
-            }
-        }
-    }
+    
     
     
     @IBAction func saveButtonPressed(_ sender: UIButton) {
@@ -125,3 +119,34 @@ class IdentifyYourselfViewController: UIViewController, AVCaptureMetadataOutputO
         delegate?.setNewName(newName)
     }
 }
+
+class CameraFrameView:UIView, AVCaptureMetadataOutputObjectsDelegate {
+    var delegate: IdentifyYourselfViewController?
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            DispatchQueue.main.async {
+                self.frame = CGRect.zero
+                self.delegate!.nameLabelOutput.text = "No QR code is detected"
+            }
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObject.ObjectType.qr {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            DispatchQueue.main.async {
+                print("I see you...")
+                if metadataObj.stringValue != nil {
+                    self.delegate!.nameLabelOutput.text = metadataObj.stringValue
+                }
+            }
+        }
+    }
+   
+}
+
