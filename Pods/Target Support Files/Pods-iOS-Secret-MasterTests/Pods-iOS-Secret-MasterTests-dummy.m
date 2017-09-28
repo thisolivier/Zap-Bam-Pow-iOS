9930ce480f7d5072cc2f94d82db53edaa468ed30
Zainab-Kalief <alesewura@yahoo.com>
2017-09-28 16:25:31 -0700

    Adding pod files

---
+++ Pods/Target Support Files/Pods-iOS-Secret-MasterTests/Pods-iOS-Secret-MasterTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Secret_MasterTests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Secret_MasterTests
+@end
---
+++ Pods/Target Support Files/Pods-iOS-Secret-MasterUITests/Pods-iOS-Secret-MasterUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOS_Secret_MasterUITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOS_Secret_MasterUITests
+@end
